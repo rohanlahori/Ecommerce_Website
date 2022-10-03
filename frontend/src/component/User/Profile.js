73562d3821895f91a2a3981a6c87989ee1f1dbc2
Rohan Lahori <rohanlahori99@gmail.com>
2022-10-04 01:21:13 +0530

    Temprory Profile Page Working

--- frontend/src/component/User/Profile.js
+++ frontend/src/component/User/Profile.js
@@ -1,73 +1,56 @@
-import React,{Fragment,useEffect} from 'react'
-import MetaData from '../layout/MetaData'
-import Loader from '../layout/Loader/Loader'
-import { Link, Navigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom';
-import "./Profile.css"
-import { TextField } from '@mui/material'
-export const Profile = () => {
-    const {user,loading,isAuthenticated}=useSelector(state=>state.user)
-    const navigate=useNavigate()
-    useEffect(()=>{
-        if(isAuthenticated==false)
-        {
-            navigate('/login')
-        }
-    },[])
+import React, { Fragment, useEffect } from "react";
+import { useSelector } from "react-redux";
+import MetaData from "../layout/MetaData";
+import Loader from "../layout/Loader/Loader";
+import { Link } from "react-router-dom";
+import "./Profile.css";
+
+export const Profile = ({  }) => {
+  const { user, loading, isAuthenticated } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (isAuthenticated === false) {
+    //   history.push("/login");
+    }
+  }, [ isAuthenticated]);
   return (
     <Fragment>
-        {
-        loading?
-        (
-            <Loader/>
-        )
-        :
-        (
-            <Fragment>
-            <MetaData title={`${user.name}'s Profile`}/>
-            <div className='profileContainer'>
-                <div>
-                    <h2>{`My Profile`} </h2>
-                    <img 
-                    src={'https://cdn.pixabay.com/photo/2022/09/22/16/51/waterfall-7472700__480.jpg'}
-                    width="40%" height="40%">
-                        
-                    </img>
-                    <Link to="/me/update">Edit Profile</Link>
-                </div>
-
-                <div>
-                    <h2>Personal Information : 
-                    </h2>
-                    
-                    <br></br>
-                    <h2>Name : {user.name} </h2>
-                    {/* <br></br><br></br> */}
-                    <h2>Email : {user.email}</h2> 
-                    <h2>Mobile No : {user.contact}</h2> 
-
-                </div>
-                <div>
-                </div>
-
-                <div>
-                    <h3>
-                    <Link to="/orders">My Orders</Link>
-                    </h3>
-                    <h3>
-                    <Link to="/password/update">Change Password</Link>
-                    </h3>
-                    <h3>
-                    <Link to="/edit/profile">Edit Profile</Link>
-                    </h3>
-                   
-                </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title={`${user.name}'s Profile`} />
+          <div className="profileContainer">
+            <div>
+              <h1>My Profile</h1>
+              <img src={'https://cdn.pixabay.com/photo/2022/09/20/10/11/street-7467503__480.jpg'} 
+              width="40%" height="40%" alt={user.name} />
+              <Link to="/me/update">Edit Profile</Link>
             </div>
-            </Fragment>
-        )
-        }
-    </Fragment>
-  )
-}
+            <div>
+              <div>
+                <h4>Full Name</h4>
+                <p>{user.name}</p>
+              </div>
+              <div>
+                <h4>Email</h4>
+                <p>{user.email}</p>
+              </div>
+              <div>
+                <h4>Joined On</h4>
+                <p>{String(user.createdAt).substr(0, 10)}</p>
+              </div>
 
+              <div>
+                <Link to="/orders">My Orders</Link>
+                <Link to="/password/update">Change Password</Link>
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default Profile;
