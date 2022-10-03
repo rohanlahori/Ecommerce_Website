512ed3a8e7a384b7077448372dd4f618d41587c6
Rohan Lahori <rohanlahori99@gmail.com>
2022-10-03 17:32:57 +0530

    Login Logout Dynamic Completed

--- frontend/src/component/Navigation/navigation_bar.jsx
+++ frontend/src/component/Navigation/navigation_bar.jsx
@@ -21,7 +21,7 @@ import store from "../../store"
 import {useNavigate} from "react-router-dom"
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 
 const pages = ['Home', 'Products', 'Contact', "About"];
@@ -29,7 +29,7 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 
 const ResponsiveAppBar = () => {
-  const{isAuthenticated}=useSelector(state=>state.user)
+  const{user,isAuthenticated}=useSelector(state=>state.user)
   console.log(isAuthenticated)
 
   const dispatch=useDispatch()
@@ -147,15 +147,19 @@ const ResponsiveAppBar = () => {
             inputProps={{ 'aria-label': 'search' }}
           />
         </Search>
-        if(!isAuthenticated)
         {
+          isAuthenticated ?
+          <Button  variant="contained" onClick={() =>dispatch(logout())}>Logout</Button>
+          :
           <Button variant="contained"  onClick={() => navigator("/login")}>Login</Button>
         }
-        else{
-          <Button variant="contained" onClick={() =>dispatch(logout())}>Logout</Button>
-        }
+        <div>
+          <h3>Welcome</h3>
+        </div>
         <Button variant="conatined" >Cart
-        <ShoppingCartIcon></ShoppingCartIcon></Button>
+        <ShoppingCartIcon></ShoppingCartIcon>
+        </Button>
+        <AccountBoxIcon fontSize='large'/>
         </Toolbar>
       </AppBar> 
       </Box>
